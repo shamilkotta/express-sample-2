c81076e52a0b89801b93efa82e58b3b79df3a433
shamilkotta <shamilkotta99@gmail.com>
2022-10-23 21:23:33 +0530

    basic route setup

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.render("home");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+})
+
 module.exports = router;
