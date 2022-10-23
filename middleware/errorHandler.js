1fadf79a0f9154ba08ee7d222859f91b83c4608f
shamilkotta <shamilkotta99@gmail.com>
2022-10-23 21:22:34 +0530

    rendering error page

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,5 @@
 module.exports = (err, req, res, next) => {
   let statusCode = 400;
   if (err.message === "Not found") statusCode = 404;
-  res.status(statusCode).send(err.message);
-  next();
+  res.status(statusCode).render("error", { error: err });
 };
