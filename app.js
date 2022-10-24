ba0b5859a80fe4cdab5bf6ef6704045fb1e4c727
shamilkotta <shamilkotta99@gmail.com>
2022-10-24 17:21:24 +0530

    express session added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const { engine } = require("express-handlebars");
+const session = require("express-session");
 require("dotenv").config();
 
 const errorHandler = require("./middleware/errorHandler");
@@ -27,6 +28,19 @@ app.use("/static", express.static(`${__dirname}/public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// session middleware
+app.use(
+  session({
+    secret: "$2a$10$bWDmg4VVnMp",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 60000,
+    }, // 1min
+  })
+);
 
 // route
 app.use("/", indexRouter);
