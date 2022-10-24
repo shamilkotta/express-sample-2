1aa44e015134c8768961839fb38ce9dd96043830
shamilkotta <shamilkotta99@gmail.com>
2022-10-24 17:21:52 +0530

    session created and redirection

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,23 @@ const loginHelper = require("../helpers/loginHelper");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
+  if (req.session.loggedIn) res.render("home");
+  else res.redirect("/login");
 });
 
 router.get("/login", (req, res) => {
-  res.render("login");
+  if (req.session.loggedIn) res.redirect("/");
+  else res.render("login");
 });
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const result = await loginHelper(email, password);
-  if (result.status) res.redirect("/");
-  else res.render("login", result);
+  if (result.status) {
+    req.session.loggedIn = true;
+    req.session.id = email;
+    res.redirect("/");
+  } else res.render("login", result);
 });
 
 module.exports = router;
